3d22aacf907e747320ea7c6c793659cff06cbb89
IJoanA <iahmed4@students.towson.edu>
2024-10-28 16:29:43 -0400

    Layout Frontend additions

--- pet-save-app-folder/app/layout.tsx
+++ pet-save-app-folder/app/layout.tsx
@@ -1,3 +1,42 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import {inter} from 'next/font/google'
+
+//header
+//sidebar
+
+import {Toaster} from 'react-hot-toast'
+
+const inter = Inter({ subsets: ['latin']}})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+    const [sidebarOpen, setSidebarOpen] = useState(false)
+    const [totalEarnings, setTotalEarnings] = useState(0)
+
+    return (
+      <html lang='en'>
+        <body className={inter.className}>
+          <div className='min-h-screen bg-gray-50 flex flex-col'>
+            {/* header */}
+              <div className='flex flex=1'>
+              {/* sidebar */}
+                <main className= 'flex-1 p-4 lg:p-8 ml-0 lg:ml-64 transition-all duration-300'
+                {children}
+                </main>
+              </div>
+          </div>
+          <Toaster/>
+        </body>
+      </html>
+    )
+    
+
+/*
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -33,3 +72,4 @@ export default function RootLayout({
     </html>
   );
 }
+*/
