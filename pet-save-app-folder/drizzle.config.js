e9b982e14ece928ac737f5bb237d31515de45324
Tayyab Ali <63280240+tayybi@users.noreply.github.com>
2024-10-28 12:19:02 -0400

    changes

--- pet-save-app-folder/drizzle.config.js
+++ pet-save-app-folder/drizzle.config.js
@@ -3,9 +3,8 @@ export default {
     schema: "./utils/db/schema.ts",
     out: "./drizzle",
     dbCredentials: {
-      url: "postgresql://neondb_owner:pd9LsYJXwZ4K@ep-quiet-king-a830o8wa.eastus2.azure.neon.tech/petsavedb?sslmode=require",
-      connectionString:
-        "postgresql://neondb_owner:pd9LsYJXwZ4K@ep-quiet-king-a830o8wa.eastus2.azure.neon.tech/petsavedb?sslmode=require",
+      url: process.env.DATABASE_URL,
+      connectionString: process.env.DATABASE_URL,
     },
   };
   
